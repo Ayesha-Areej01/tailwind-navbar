d20455dc0a4642b575eb90b8cced1ce3ea750df4
Ayesha-Areej01 <areej95smiles@gmail.com>
2024-02-10 21:27:33 +0500

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+  return (
+    <div>
+    <main className="bg-white flex min-h-screen text-6xl font-bold flex-col text-center justify-center items-center p-28">
+      Landing Page
+    </main>
+    <footer>
+    <div className="bg-slate-700 border border-[#33353F] text-center py-4 pb-16 md:pb-4">
+      Copyright © ABC <br /> All Rights Reserved 2023.
+    </div>
+    </footer>
+    </div>
+  );
+}
